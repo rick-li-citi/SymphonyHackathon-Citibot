f698696fca4a0f694b75566d04e5e9487b45cbce
Rick Li <rick.li@citi.com>
2017-10-03 13:50:54 -0400

    clean up

--- src/main/webapp/js/registerApplication.js
+++ src/main/webapp/js/registerApplication.js
@@ -44,6 +44,11 @@ const registerModule = (config) => {
     const entityService = SYMPHONY.services.subscribe('entity');
     let msgType = 'com.symphony.integration.zapier.event.v2.searchMessage';
     entityService.registerRenderer(msgType, {}, controllerName);
+    const chartMap = {
+        'ibm': 'CREDIT.BOND.US459200HK05.PRICE',
+        'google': 'CREDIT.BOND.US38259PAB85.PRICE',
+        'apple': 'CREDIT.BOND.US037833CF55.PRICE'
+    }
     controllerService.implement({
         action: _.throttle(function(data){
             const dialogsService = SYMPHONY.services.subscribe('dialogs');
@@ -57,16 +62,6 @@ const registerModule = (config) => {
                     canFloat: true
                 }
             );
-            // dialogsService.show('test', controllerName, `
-            // <dialog>
-            //     <h1>Comment</h1>
-            //     <multiline-input id="comment" placeholder="Add your comment..."/>
-            //     <div>
-            //         <action id="closeCommentDialog" class="tempo-text-color--link"/>
-            //         <action id="commentIssue" class="tempo-text-color--link"/>
-            //     </div>
-            // </dialog>
-            // `, {}, {});
             
         }, 300),
         render(type, data) {
@@ -74,8 +69,6 @@ const registerModule = (config) => {
             if (type == msgType) {
                 
                 let resultJson = JSON.parse(data.data).Results;
-                // console.log('======',resultJson);
-                // let resultJson = [];
                 let resultML = resultJson.map( article => {
                     return `
                     <card class="barStyle" accent="tempo-bg-color--green" iconSrc="http://rick-li.ngrok.io/citibot/apps/citibot/img/bigicons_bigicon_doc.svg.png">
@@ -114,95 +107,7 @@ const registerModule = (config) => {
                     </messageML>`, data: {
                         charting: {label: 'Charting', service: controllerName, data: {searchContent: 'ibm'}}
                 }};
-                // return {
-                //     template: `
-                //     <messageML>
-                   
-                //         <div>
-                //             <a class="tempo-text-color--link" href="www.google.com">xxxx</a>
-                //                 <span>Author</span>
-                //                 <span class="tempo-text-color--blue">cccc</span> 
-                //         </div>
-                   
-                //     <div>
-                        
-                //         <span class="tempo-text-color--secondary">Description:</span>
-                        
-                //         <span class="tempo-text-color--normal">dccc</span>
-
-                //     <br/>
-                    
-                // </div>
-                // <hr/>
-                // <div>
-                // <action id="assignTo" class="tempo-btn--primary"/>
-                // </div>
-                
-                
-                    
-                //     </messageML>
-                //     `,
-                //     data: {assignTo: {label: 'Chart', service: controllerName, data: {a: 111}}}
-                // }
-                // return {
-                //     // template: `<messageML>
-                //     //     <div >hello world</div>
-                //     //     <iframe height="200" width="400" src="https://uat.citivelocity.com/analytics/charting3/?allowCross=false" />
-                        
-                //     // </messageML>`,
-                //     template: `
-                //     <messageML>
-                //     <div class="entity" >
-                //     <card class="barStyle" accent="tempo-bg-color--green" iconSrc="https://cdn1.iconfinder.com/data/icons/logotypes/32/chrome-32.png">
-                //         <header>
-                //             <div>
-                //                 <img src="http://rick-li.ngrok.io/citibot/apps/citibot/img/bigicons_bigicon_doc.svg.png" class="tempo-icon--document" />
-                                
-                //                 <a class="tempo-text-color--link" href="www.google.com">hello google</a>
-                //                     <span class="tempo-text-color--normal">Subject is  - </span>
-                //                     <span>User</span>
-                //                     <span class="tempo-text-color--green">action</span>
-                                
-                //             </div>
-                //         </header>
-                //         <body>
-                //             <div class="labelBackground badge">
-                //                 <div>
-                //                         <span class="tempo-text-color--secondary">Description:</span>
-                //                         <span class="tempo-text-color--normal">xxxxxxxxx</span>
-                //                     <br/>
-                //                     <span class="tempo-text-color--secondary">Assignee:</span>
-                //                         <mention email="racke1983cn@gmail.com" />
-                //                 </div>
-                //                 <hr/>
-                //                 <div>
-                //                     <div>
-                //                     <img src="https://uat.citivelocity.com/analytics/eppublic/chartingbe/images/a413e76d-0069-432f-9265-e8d3520fb837.png"/>
-                //                     </div>
-                //                     <div>
-                //                         <span class="tempo-text-color--secondary">&#160;&#160;&#160;Epic:</span>
-                //                         <a href="http://google.com">google</a>
-                //                     <span class="tempo-text-color--secondary">&#160;&#160;&#160;Status:</span>
-                //                     <span class="tempo-bg-color--red tempo-text-color--white tempo-token">
-                //                         testtesttest
-                //                     </span>
-            
-            
-                                    
-                //                         <span class="tempo-text-color--secondary">&#160;&#160;&#160;Labels:</span>
-                                        
-                //                             <span class="hashTag">#ddd</span>
-                //                         </div>
-                                    
-                //                 </div>
-                //             </div>
-                //         </body>
-                //     </card>
-                // </div>
-                // </messageML>
-                //     `,
-                //     data: {}
-                // };
+               
             }
         },
         link() {
@@ -215,17 +120,6 @@ const registerModule = (config) => {
         },
         trigger() {
            
-            // modulesService.show(
-            //     config.appId, {
-            //         title: config.appTitle
-            //     },
-            //     controllerName,
-            //     'https://uat.citivelocity.com/analytics/charting3/?allowCross=false', {
-            //         canFloat: true
-            //     }
-            // );
-
-                console.log('===config ', config );
               const url = [
                 `${params.host}/citibot/${config.appContext}/app.html`,
                 `?configurationId=${params.configurationId}`,
